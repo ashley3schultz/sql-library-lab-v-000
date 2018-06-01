36427963ede848ea73275061ff05bd754159b100
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 02:43:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name COUNT(characters.name) FROM characters JOIN series ON series.id = characters.series_id JOIN books ON books.series_id = characters.series_id WHERE characters.series_id = books.series_id"
 end
