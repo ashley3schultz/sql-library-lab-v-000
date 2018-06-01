bb0832d533bd4a1a53dcc4e053bd9d746d4265ce
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 01:54:38 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -27,3 +27,13 @@ INSERT INTO subgenres (name) VALUES
 INSERT INTO authors (name) VALUES
 ('Sylvain Neuvel'),
 ('Justin Cronin');
+
+INSERT INTO character_books (character_id, book_id) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 2),
+(6, 2),
+(7, 2),
+(8, 2);
