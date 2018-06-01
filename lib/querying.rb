7249b287dae5ea238c33e0b1c5df820e7742b0d3
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 02:57:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(characters.name) FROM series
-   JOIN characters ON series.id = characters.series_id
    JOIN books ON books.series_id = characters.series_id
    WHERE books.series_id = characters.series_id GROUP BY characters.name"
 end
