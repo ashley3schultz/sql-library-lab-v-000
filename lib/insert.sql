28847e672b387802a6c38994a8471bfc85a21dd2
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-01 01:49:03 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,29 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES 
+('The Themis Files' 1, 1),
+('The Passage Tilogy');
+
+INSERT INTO books (title, year, series_id) VALUES
+('Sleeping Giants', 2016, 1),
+('Waking Gods', 2017, 1),
+('Only Human', 2018, 1),
+('The Passage', 2010, 2),
+('The Twelve', 2012, 2),
+('The City Of Mirrors', 2016, 2);
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES
+('name1', 'species1', 'motto1', 1, 1),
+('name2', 'species2', 'motto2', 1, 1),
+('name3', 'species3', 'motto3', 1, 1),
+('name4', 'species4', 'motto4', 1, 1),
+('name5', 'species5', 'motto5', 2, 2),
+('name6', 'species6', 'motto6', 2, 2),
+('name7', 'species7', 'motto7', 2, 2),
+('name8', 'species8', 'motto8', 2, 2);
+
+INSERT INTO subgenres ()
+('Science Fiction'),
+('Justin Cronin');
+
+INSERT INTO authors ()
+(Sylvain Neuvel),
+();
